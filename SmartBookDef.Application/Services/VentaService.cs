8adaefce0924592abf420ac0b3bdb2a99acde556
daniel <danielperezjaraba2020@gmail.com>
2025-11-26 03:31:34 -0500

    cambio 01 (Usuario ahora debe existir para crear la venta)

--- SmartBookDef.Application/Services/VentaService.cs
+++ SmartBookDef.Application/Services/VentaService.cs
@@ -26,15 +26,15 @@ public class VentaService
 
     public CrearVentaResponse CrearVentaYEnviarPdf(CrearVentaRequest request)
     {
+        if (request.Libros == null || !request.Libros.Any())
+        {
+            throw new BussinesRuleException("Debe incluir al menos un libro en la venta");
+        }
+
         var usuario = _ventaRepository.Usuario(request.Usuario);
         if (string.IsNullOrEmpty(usuario))
         {
             throw new BussinesRuleException("No se encontró un Usuario con esta Identificación");
-        }
-
-        if (request.Libros == null || !request.Libros.Any())
-        {
-            throw new BussinesRuleException("Debe incluir al menos un libro en la venta");
         }
 
         foreach (var item in request.Libros)
